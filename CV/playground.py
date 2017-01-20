246937523b771556b9f2b8d68c937075b8fd6fc0
Sdpspot <Sdpspot@gmail.com>
2017-01-20 04:56:30 +0000

    Python environment for testing raspicam/openalpr integration

---
+++ CV/playground.py
@@ -0,0 +1,30 @@
+#--------------------------------------------------------------------------------------------------
+#   Desc : A script to play around with ALPR and RaspiCam integration - 0xDC
+#   Notes: Originally ported from binding examples: doc.openalpr.com/bindings.html
+#--------------------------------------------------------------------------------------------------
+from openalpr import Alpr
+
+alpr = Alpr("us", "./config/openalpr.conf", "./runtime_data")
+if not alpr.is_loaded():
+    print("Error loading OpenALPR")
+    sys.exit(1)
+
+alpr.set_top_n(20)
+alpr.set_default_region("md")
+
+results = alpr.recognize_file("/home/pi/Downloads/Test_GoogleCar.jpg")
+
+i = 0
+for plate in results['results']:
+    i += 1
+    print("Plate #%d" % i)
+    print("   %12s %12s" % ("Plate", "Confidence"))
+    for candidate in plate['candidates']:
+        prefix = "-"
+        if candidate['matches_template']:
+            prefix = "*"
+
+        print("  %s %12s%12f" % (prefix, candidate['plate'], candidate['confidence']))
+
+# Call when completely done to release memory
+alpr.unload()
