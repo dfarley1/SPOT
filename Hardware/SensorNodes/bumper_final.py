7e2eaf35032dd9f92120a11e8fda1bf40d0fb045
Erik Jung <erikjung@Eriks-MacBook-Pro.local>
2017-01-29 18:54:33 -0800

    Bumper update

--- Hardware/SensorNodes/bumper_final.py
+++ Hardware/SensorNodes/bumper_final.py
@@ -1,19 +1,8 @@
-# bumper_detector_final.py
-# Measure distance using an ultrasonic module
-#
-# Author : Erik Jung
-# Date   : 01/26/2017
-
-# -----------------------
-# Import required Python libraries
-# If libraries not installed into RPI devices, install them.
-# Check README for more details.
-# -----------------------
-import time
 import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM)
 
-TRIG = 23
-
+TRIG = 23 
 ECHO = 24
 
 print "Distance Measurement In Progress"
@@ -22,22 +11,18 @@ GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
 GPIO.output(TRIG, False)
-
 print "Waiting For Sensor To Settle"
-
 time.sleep(2)
 
 GPIO.output(TRIG, True)
-
 time.sleep(0.00001)
-
 GPIO.output(TRIG, False)
 
 while GPIO.input(ECHO)==0:
   pulse_start = time.time()
 
 while GPIO.input(ECHO)==1:
-  	pulse_end = time.time() 
+  pulse_end = time.time()
 
 pulse_duration = pulse_end - pulse_start
 
