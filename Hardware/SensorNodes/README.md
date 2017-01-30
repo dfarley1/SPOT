f1493e4b10b32ac2cb1cb25697adbf35c6f3be7b
Erik Jung <erikjung@Eriks-MacBook-Pro.local>
2017-01-29 19:33:47 -0800

    Removed bumper detector file

--- Hardware/SensorNodes/README.md
+++ Hardware/SensorNodes/README.md
@@ -1,5 +1,19 @@
 This is a README for the ping sensor setup.
 
+For new raspberry pi units, the hardware must be updated. 
+
+sudo python
+>>import RPi.GPIO as GPIO
+>>GPIO.VERSION
+>>exit
+
+If device version less than 0.54, update:
+sudo apt-get install update
+sudo apt-get install upgrade
+
+Reference: http://makezine.com/projects/tutorial-raspberry-pi-gpio-pins-and-python/
+
+
 GPIO Pins 23 and 24 are designated for operating the HC-SR04 ultrasonic ping sensor. Trigger will be used to enable the ping sensor for measuring distances, whereas echo will be used as an input into the GPIO pins on the Raspberry Pi 3 to find distance. The ground pins and Vcc pins on the HC-SR04 will be connected via the Vcc & GND pin on the RPI.
 
 Output
@@ -11,3 +25,5 @@ GPIO-24: ECHO
 The file will return a value in centimeters, can change for different units. 
 
 Will need to follow circuit diagram posted in the SPOT Document for optimal results. 
+
+Reference to the details above come from this website: https://www.modmypi.com/blog/hc-sr04-ultrasonic-range-sensor-on-the-raspberry-pi
--- Hardware/SensorNodes/bumper_final.py
+++ Hardware/SensorNodes/bumper_final.py
@@ -1,5 +1,57 @@
+
+
+
+#!/usr/bin/python
+#
+# bumper_final.py
+# Measure distance using an ultrasonic module.
+#
+# Author : Erik Jung
+# Date   : 01/29/2017
+
+# -----------------------
+# Import required Python libraries
+# If libraries not installed into RPI devices, install them.
+# Check README for more details.
+# -----------------------
+
 import RPi.GPIO as GPIO
 import time
+
+# -----------------------
+# Define some functions
+# -----------------------
+
+def measure():
+  # This function measures a distance
+  GPIO.output(GPIO_TRIGGER, True)
+  time.sleep(0.00001)
+  GPIO.output(GPIO_TRIGGER, False)
+  start = time.time()
+
+  while GPIO.input(GPIO_ECHO)==0:
+    start = time.time()
+
+  while GPIO.input(GPIO_ECHO)==1:
+    stop = time.time()
+
+  elapsed = stop-start
+  distance = (elapsed * 34300)/2
+
+  return distance
+
+def measure_average():
+  # This function takes 3 measurements and
+  # returns the average.
+  distance1=measure()
+  time.sleep(0.1)
+  distance2=measure()
+  time.sleep(0.1)
+  distance3=measure()
+  distance = distance1 + distance2 + distance3
+  distance = distance / 3
+  return distance
+
 GPIO.setmode(GPIO.BCM)
 
 TRIG = 11 
@@ -29,7 +81,20 @@ pulse_duration = pulse_end - pulse_start
 distance = pulse_duration * 17150
 
 distance = round(distance, 2)
-
 print "Distance:",distance,"cm"
+
+
+try:
+	while True:
+		distance = measure_average()
+		print "Distance:",distance,"cm"
+		time.sleep(1)
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
+
+
+
+
 
 GPIO.cleanup()
