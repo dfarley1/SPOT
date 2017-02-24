37462667a6571ec6716a0fd4a71a923a41a5951a
Dave <dc11011100@gmail.com>
2017-02-24 22:25:54 +0000

    Updated Sensor code

---
+++ scripts/crontab/crontabby.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#run the command
+~/CV/scripts/photo_triggers/ping_trigger.sh
+
+#wait for 30 seconds
+sleep 30
+
+#run the command
+ ~/CV/scripts/photo_triggers/ping_trigger.sh
+
---
+++ scripts/setup/sensor_nodes/bootup_ping_script.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+PING_CHECKER="nohup /home/pi/SPOT/CV/scripts/photo_triggers/background_ping.sh &"
+
+$PING_CHECKER
+
