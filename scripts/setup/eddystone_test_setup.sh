0f9c85bea2d545a644be139f91be9494b458bcd3
Lize Calderon <liclader@ucsc.edu>
2017-02-24 01:12:54 -0800

    eddystone new url

--- scripts/setup/eddystone_test_setup.sh
+++ scripts/setup/eddystone_test_setup.sh
@@ -2,4 +2,4 @@
 
 sudo hciconfig hci0 up
 sudo hciconfig hci0 leadv 3
-sudo hcitool -i hci0 cmd 0x08 0x0008 17 02 01 06 03 03 aa fe 0f 16 aa fe 10 00 03 77 65 62 67 61 7a 65 72 08 00 00 00 00 00 00 00 00
+sudo hcitool -i hci0 cmd 0x08 0x0008 16 02 01 06 03 03 aa fe 0e 16 aa fe 10 00 03 73 64 70 73 70 6f 74 07 00 00 00 00 00 00 00 00 00
