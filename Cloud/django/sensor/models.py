bbc8b60c4a690fe014e54a52fadafe2ba29f61fd
Daniel Farley <danfarley93@gmail.com>
2017-04-27 23:33:10 -0700

    2/3 sensor/models.py, still works

--- Cloud/django/sensor/models.py
+++ Cloud/django/sensor/models.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#	 http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,52 +18,54 @@ import uuid
 from authentication.models import Account, AccountManager
 
 class spot_data(models.Model):
-	uuid = models.UUIDField("UUID", primary_key = True, unique = True)
-	#"static" metadata about spot
-	#structure = models.OneToOneField('spot_structs', on_delete=models.CASCADE, default=None)
-	active = models.BooleanField("Active", default=True)
-	section = models.CharField("Section", max_length=20)
-	number = models.IntegerField("Spot Number")
-	description = models.CharField("Description", max_length=50)
-	gpslat = models.FloatField("Latitude", null=True)
-	gpslon = models.FloatField("Longitude", null=True)
-	#"variable" current status
-	last_update = models.DateTimeField("Last Update", auto_now=True)
-	occ_status = models.SmallIntegerField("Occupied Status", default=0)
-	occ_since = models.DateTimeField("Occupied Since")
-	occ_license = models.CharField("Occupant License", max_length=20)
+    uuid = models.UUIDField("UUID", primary_key = True, unique = True)
+    #"static" metadata about spot
+    #structure = models.OneToOneField('spot_structs', on_delete=models.CASCADE, default=None)
+    active = models.BooleanField("Active", default=True)
+    section = models.CharField("Section", max_length=20)
+    number = models.IntegerField("Spot Number")
+    description = models.CharField("Description", max_length=50)
+    gpslat = models.FloatField("Latitude", null=True)
+    gpslon = models.FloatField("Longitude", null=True)
+    #"variable" current status
+    last_update = models.DateTimeField("Last Update", auto_now=True)
+    occ_status = models.SmallIntegerField("Occupied Status", default=0)
+    occ_since = models.DateTimeField("Occupied Since")
+    occ_license = models.CharField("Occupant License", max_length=20)
 
-	
-	occupant = models.ForeignKey(Account, null=True, related_name='spot_occupied')
-	
-	def __str__(self):
-		return (str(self.uuid))
+    occupant = models.ForeignKey(Account, null=True, related_name='spot_occupied')
+
+    def __str__(self):
+        return str(self.uuid)
+
+    def pretty_str(self):
+        return str(self.section) + " " + str(self.number)
 
 class spot_data_form(ModelForm):
-	class Meta:
-		model = spot_data
-		fields = ['active', 'section', 'number', 
-				'description', 'gpslat', 'gpslon']
+    class Meta:
+        model = spot_data
+        fields = ['active', 'section', 'number',
+                  'description', 'gpslat', 'gpslon']
 
 
 
 
 class spot_structs(models.Model):
-	name = models.CharField("Structure", max_length=100)
-	def __str__(self):
-		return str(self.name)
-	
+    name = models.CharField("Structure", max_length=100)
+    def __str__(self):
+        return str(self.name)
+    
 class spot_structs_form(ModelForm):
-	class Meta:
-		model = spot_structs
-		fields = ['name']
+    class Meta:
+        model = spot_structs
+        fields = ['name']
 
 
 
 
 class spot_sections(models.Model):
-	name = models.CharField("Section", max_length=100)
+    name = models.CharField("Section", max_length=100)
 class spot_sections_form(ModelForm):
-	class Meta:
-		model = spot_sections
-		fields = ['name']
+    class Meta:
+        model = spot_sections
+        fields = ['name']
